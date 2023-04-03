f54aaf8d136b8674f929e16dd4b5235013988f71
Sebastian Zapart <259383@stud.inn.no>
2023-04-03 13:35:41 +0200

    Test Animation Workign

--- ExamGame/Source/ExamGame/Player_AnimInstance.h
+++ ExamGame/Source/ExamGame/Player_AnimInstance.h
@@ -13,7 +13,6 @@ UCLASS()
 class EXAMGAME_API UPlayer_AnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-
 		
 
 public: 
@@ -23,47 +22,71 @@ public:
 	virtual void NativeInitializeAnimation() override;
 
 	UFUNCTION(BlueprintCallable)
-	void NativeUpdateAnimation(float DeltaTime) override;
+		void UpdateAnimationProperties();
 
 	UFUNCTION(BlueprintCallable)
-		void CombatAnimReset();
-	// ------------------------ Pointer and references ----------------------------
+		void ResetAttack();
+
+	//Animations cannot dictate verriables so make sure it is read only
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+		float MovementSpeed;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	//	float IsAttack;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
 		class APawn* Pawn;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
 		class APlayer_Character* Player;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
-		class ACharacter* Character;
+
+
+
+
+	/*UFUNCTION(BlueprintCallable)
+	void NativeUpdateAnimation(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+		void CombatAnimReset();*/
+	// ------------------------ Pointer and references ----------------------------
+
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	//	class APawn* Pawn;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	//	class APlayer_Character* Player;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	//	class ACharacter* Character;
 
 
 	// ------------------------ Functions ----------------------------
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Variables")
-		float MovementSpeed;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Variables")
+	//	float MovementSpeed;
 
 	// ------------------------ Booleans---------------------------
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
-		bool AxeAnim;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
+	//	bool AxeAnim;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
-		bool WalkAnim;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
+	//	bool WalkAnim;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
-		bool SprintAnim;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
+	//	bool SprintAnim;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
-		bool FallingIdleAnim;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
+	//	bool FallingIdleAnim;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
-		bool FallingEndAnim;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
+	//	bool FallingEndAnim;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
-		bool ExhaustAnim;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
+	//	bool ExhaustAnim;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
-		bool CrouchAnim;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
+	//	bool CrouchAnim;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
-		bool JumpAnim;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Booleans")
+	//	bool JumpAnim;
 };
