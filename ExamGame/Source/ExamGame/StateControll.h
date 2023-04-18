bd36771e42046218aa78e775ebb18db240c5b648
Sebastian Zapart <259383@stud.inn.no>
2023-04-18 19:36:14 +0200

    Complete Enemy one

--- ExamGame/Source/ExamGame/StateControll.h
+++ ExamGame/Source/ExamGame/StateControll.h
@@ -6,6 +6,7 @@ enum class EEnemyState : uint8
 	EES_EnemyIdle UMETA(DisplayName = "Idle"),
 	EES_EnemyPatrolling UMETA(DisplayName = "Patrolling"),
 	EES_EnemyChaseing UMETA(DisplayName = "Chaseing"),
+	EES_EnemyReadyToAttack UMETA(DisplayName = "ReadyToAttack"),
 	EES_EnemyAttacking UMETA(DisplayName = "Attacking"),
 
 };
